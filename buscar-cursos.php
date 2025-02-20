4e929421008ef05146086e322cf4e64861176424
Erivelton Santos <erivelton.santosj5@gmail.com>
2025-02-20 16:13:31 -0300

    First commit

---
+++ buscar-cursos.php
@@ -0,0 +1,19 @@
+<?php
+require 'vendor/autoload.php';
+require 'src/buscador.php';
+
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCurso\Buscador;
+
+$client = new Client(['base_uri'=>'https://www.alura.com.br/']);
+
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar(url:'/cursos-online-programacao/php');
+
+foreach ($cursos as $curso){
+    echo exibirMensagem($curso);
+};
